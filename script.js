e7cbbce98ab6acc51bbdbdab4225f5bd78c4e737
Влад Билоконь <vlad2004bil@gmail.com>
2023-04-29 17:45:07 +0300

    loop switch

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let submit = document.getElementById("submit");
 let registration = document.getElementById("registration");
 let clos = document.getElementById("close");
-let switchMode = document.getElementById("switchMode")
+let switchMode = document.getElementById("switchMode");
 
 window.onscroll = function () {
-    scrollFunction()
+    scrollFunction();
 }
 
 function scrollFunction() {
@@ -18,11 +18,11 @@ function scrollFunction() {
 
 
 registration.onclick = function () {
-    popup.classList.remove("hide")
+    popup.classList.remove("hide");
 }
 
 clos.onclick = function () {
-    popup.classList.add("hide")
+    popup.classList.add("hide");
 }
 
 submit.onclick = function () {
@@ -30,14 +30,26 @@ submit.onclick = function () {
     let Last_Name = document.querySelector(".LName").value;
     let PWord = document.querySelector(".PWord").value;
 
-    confirmation = confirm(` Ваше ім'я: ${Name};  \n Ваше прізвище: ${Last_Name}; \n Ваш пароль: ${PWord}; `)
+    confirmation = confirm(` Ваше ім'я: ${Name};  \n Ваше прізвище: ${Last_Name}; \n Ваш пароль: ${PWord}; `);
     if (confirmation == true) {
-        location.href = "index.html"
+        location.href = "index.html";
     }
 }
 
 switchMode.onclick = function () {
-    let body = document.querySelector("body")
-    body.style.backgroundColor = "#0f5c76"
-    setTimeout(() => body.style.background = "rgb(238, 238, 238)", 3000);
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "#0f5c76";
+    setTimeout(() => body.style.background = "", 3000);
+}
+
+function FuncString() {
+    let f_string = prompt("Введіть перший рядок для порівняння");
+    let s_string = prompt("Введіть другий рядок для порівняння");
+    if (f_string > s_string) {
+        alert(`Перший рядок "${f_string}" більше за другий "${s_string}"`)
+    } else if (f_string < s_string) {
+        alert(`Перший рядок "${f_string}" менше за другий "${s_string}"`)
+    } else {
+        alert(`Перший рядок "${f_string}" дорівнює другому "${s_string}"`)
+    }
 }
