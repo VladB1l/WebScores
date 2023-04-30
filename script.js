f834caddc0a81d4b52e1c7b6920c682d761e1621
Влад Билоконь <vlad2004bil@gmail.com>
2023-04-30 03:53:47 +0300

    Full Lab #6

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let submit = document.getElementById("submit");
 let registration = document.getElementById("registration");
 let clos = document.getElementById("close");
 let switchMode = document.getElementById("switchMode");
-
+let popup = document.getElementById("popup");
+let scroll_top = document.getElementById("scroll_top");
+let main = document.querySelector("main")
 
 
 window.onscroll = function () {
@@ -10,22 +12,22 @@ window.onscroll = function () {
 }
 
 function scrollFunction() {
-    let top = document.getElementById("top")
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        top.classList.remove("hide");
+        scroll_top.hidden = false
     } else {
-        top.classList.add("hide");
+        scroll_top.hidden = true
     }
 }
 
 
 registration.onclick = function () {
-    popup.classList.remove("hide");
+    popup.hidden = false
 }
 
 clos.onclick = function () {
-    popup.classList.add("hide");
+    popup.hidden = true
 }
+
 
 submit.onclick = function () {
     let Name = document.querySelector(".FName").value;
@@ -39,10 +41,9 @@ submit.onclick = function () {
 }
 
 switchMode.onclick = function () {
-    let body = document.querySelector("body");
-    body.style.backgroundColor = "#0f5c76";
-    setTimeout(() => body.style.background = "", 3000);
-    
+    document.body.style.backgroundColor = "#0f5c76";
+    setTimeout(() => document.body.style.background = "", 3000);
+
 }
 
 function FuncString() {
@@ -55,20 +56,61 @@ function FuncString() {
     // } else {
     //     alert(`Перший рядок "${f_string}" дорівнює другому "${s_string}"`)
     // }
-    f_string > s_string ?  alert(`Перший рядок "${f_string}" більше за другий "${s_string}"`) :
-    f_string < s_string ?  alert(`Перший рядок "${f_string}" менше за другий "${s_string}"`) :  alert(`Перший рядок "${f_string}" дорівнює другому "${s_string}"`);
+    f_string > s_string ? alert(`Перший рядок "${f_string}" більше за другий "${s_string}"`) :
+        f_string < s_string ? alert(`Перший рядок "${f_string}" менше за другий "${s_string}"`) : alert(`Перший рядок "${f_string}" дорівнює другому "${s_string}"`);
 }
 
-function FuncInner(){
-    for(let element of elements){
+function FuncInner() {
+    for (let element of elements) {
         element.innerHTML = prompt("Ви можете змінити текст заголовку цього сайту", "WebScores")
     }
 }
 
-function FuncOuter(){
+function FuncOuter() {
     let selector = prompt("Напишіть селектор CSS елемента, якого ви хочите подивитися код");
     let variables = document.querySelectorAll(selector);
     for (let variable of variables) {
-      alert(variable.outerHTML);  
+        alert(variable.outerHTML);
+    }
+}
+
+function FuncTextContent() {
+    alert(main.textContent)
+}
+
+
+function FuncNodeValue() {
+    alert(main.childNodes[1].nodeValue)
+}
+
+let new_button = document.createElement('button');
+new_button.innerHTML = `<img src="./Img/object.png" alt="object">`
+document.body.firstElementChild.children[1].prepend(new_button)
+
+
+let textNode = document.createTextNode(".");
+main.children[1].append(textNode);
+
+
+new_button.onclick = function () {
+    new_span = document.createElement("span")
+    new_span.innerHTML = 'Доданий span'
+    new_span.style.color = "white"
+    new_span.style.padding = "25px"
+    new_span.style.cursor = "pointer"
+    new_button.before(new_span)
+
+    new_span.onclick = function () {
+        let span_collection = document.querySelectorAll("body span")
+        span_collection.length > 1 ? FuncRemove(span_collection) : FuncReplace();
+    }
+    function FuncReplace() {
+        new_span.replaceWith("Замінений текст")
+    }
+    
+    function FuncRemove(span_collection) {
+        for (const span of span_collection) {
+            span.remove();
+        }
     }
 }
